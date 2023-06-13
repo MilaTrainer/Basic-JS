0c7a7b06b7d13be124b19776cb8a0649f1c2cbfe
MilaTrainer <lyudmilaalekhinakz@gmail.com>
2023-06-13 11:38:58 -0700

    js

---
+++ js/home_works/1_leasson/script.js
@@ -0,0 +1,8 @@
+'use strict';
+// Задание 1
+// Запросите у пользователя его имя. Выведите в окошке (с помощью функции alert) 
+// фразу: Привет, Николай! (вместо Николай должно показываться имя, которое ввел пользователь)
+
+let userName = prompt ('Please enter your username');
+let showHello = 'Hi ' + userName;
+alert (showHello);
