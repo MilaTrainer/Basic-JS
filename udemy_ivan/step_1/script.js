20b09b5512f3385c2704cc4510e702eac11afcbc
MilaTrainer <lyudmilaalekhinakz@gmail.com>
2023-08-29 18:59:46 -0700

    loops_ more

--- udemy_ivan/step_1/script.js
+++ udemy_ivan/step_1/script.js
@@ -25,21 +25,76 @@
 // Задайте в коде переменную n с числовым значением.
 
 // С помощью логического И в условии проверьте, находится ли переменная n в диапазоне чисел от 0 до 100 включительно.
-let numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 const personalMovieDB = {
     count: 0,
+    // или можно сразу записать переменную в значение ключа
+    // count: numberOfFilms
     movies: {},
     actors: {},
     genres: [],
     privat: false
+};
+
+personalMovieDB.count = numberOfFilms;
+console.log(personalMovieDB);
+
+const qwestionUser = prompt('Один из последних просмотренных фильмов?', '');
+const ratingFilm = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[qwestionUser] = ratingFilm; //изменила название в обьектк ключа  и присвоила значение
+// когда мы записыаем какое- то свойсво в обьект, лучще использовать [квадратные скобки]:
+// 1.так избовляемся от багов, которые пользлватель мог ввести
+// 2. есть некоторые баги - в некоторых версиях, которая через точку не работают
+
+console.log(personalMovieDB);
+
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+
+// 2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+// отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+// возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
+// str.length - и получить её длину)
+
+function create() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', '');
+        const b = prompt('На сколько оцените его?', '');
+
+        if ((a !== '' && b !== '') || (a !== null && b !== null) || (a.length < 50)) {
+            personalMovieDB.movies[a] = b;
+            console.log('yes, it true');
+        }
+        else {
+            console.log('Please repead')
+        }
+        return
+    }
 }
 
-personalMovieDB.count = numberOfFilms; 
-console.log(personalMovieDB);
+create();
 
-let qwestionUser = prompt ('Один из последних просмотренных фильмов?', '');
-let ratingFilm = prompt ('На сколько оцените его?', '');
+// 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+// "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+// "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-personalMovieDB.movies[qwestionUser]= ratingFilm; //изменила название в обьектк ключа  и присвоила значение
-
-console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов')
+}
+else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+}
+else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+}
+else {
+    console.log('Произошла ошибка');
+}
