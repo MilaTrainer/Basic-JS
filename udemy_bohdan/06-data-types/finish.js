771478c89cdadea3701b33b4d8d9ca5a71dd763f
Mila Alekhina <lyudmilaalekhinakz@gmail.com>
2024-02-17 23:51:07 +0100

    js

--- udemy_bohdan/06-data-types/finish.js
+++ udemy_bohdan/06-data-types/finish.js
@@ -12,3 +12,22 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const string = "string";
+const number = 1;
+const boolean = false;
+const anKnow = null;
+const undefin = undefined;
+const array = [1, 2, 3, 4];
+const object = {
+    name: 'Mila',
+    age: '29',
+};
+
+console.log(typeof (object));
+console.log(typeof (string));
+console.log(typeof (array));
+console.log(typeof (boolean));
+console.log(typeof (number));
+console.log(typeof (anKnow));
+console.log(typeof (undefin));
