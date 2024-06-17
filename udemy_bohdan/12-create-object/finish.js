4472714ba7358ade7db71d7020a6ca8681a3219b
Mila <121246297+MilaTrainer@users.noreply.github.com>
2024-06-17 04:57:53 -0700

    Add files via upload

---
+++ udemy_bohdan/12-create-object/finish.js
@@ -0,0 +1,18 @@
+/** ЗАДАЧА 12 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+const object = {
+    name: 'Mila',
+    surname: 'Bondarenko',
+    favoriteNumber: 25
+}
+
+console.log(`My name is ${object.name} and my favorite number is ${object.favoriteNumber}`);
---
+++ udemy_bohdan/12-create-object/start.js
@@ -0,0 +1,19 @@
+// /** ЗАДАЧА 12 - Создание объекта
+//  *
+//  * 1. Создайте объект с тремя свойствами:
+//  *  - name
+//  *  - surname
+//  *  - favoriteNumber
+ 
+//  * 2. Выведите в консоль строку
+//  "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+//  */
+
+const names = {
+name: 'Mila',
+surname: 'Bomdarenko',
+favoriteNumber: 25,
+}
+
+console.log(`My name is ${names.name} ${names.surname} and my favorite number is ${names.favoriteNumber}`)
+
